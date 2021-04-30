061053c6166c7800eb1fb8965b0c62028c339305
Parker Finch <finch.parker@gmail.com>
2021-04-30 15:17:13 -0400

    Remove related refs from magit commits
    There are a lot of related refs and I'm not sure what the benefit of
    them is. Setting this to `nil` gets rid of them, which makes commits
    easier to view.

--- init.el
+++ init.el
@@ -142,6 +142,7 @@
   :config
   (setq git-commit-summary-max-length 50)
   (setq magit-log-section-commit-count 20) ;; Increase from default of 10
+  (setq magit-revision-insert-related-refs nil) ;; Don't show related refs
   (add-hook 'git-commit-setup-hook (lambda () (set-fill-column 72))))
 
 (use-package forge
